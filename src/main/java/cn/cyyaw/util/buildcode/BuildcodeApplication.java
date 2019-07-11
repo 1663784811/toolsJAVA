2f40c3129e1d271d909398dc9084e767b84a07b0
gongmin <1663784811@qq.com>
2019-07-11 12:53:22 +0800

    初始化

---
+++ src/main/java/cn/cyyaw/util/buildcode/BuildcodeApplication.java
@@ -0,0 +1,13 @@
+package cn.cyyaw.util.buildcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildcodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuildcodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/cyyaw/util/buildcode/BuildcodeApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.cyyaw.util.buildcode;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BuildcodeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
