07284c874836ec5af70d764d0076dc3fb7097928
why <1663784811@qq.com>
2020-05-15 02:23:09 +0800

    升级版本

--- src/main/java/cn/cyyaw/util/buildcode/controller/CodeController.java
+++ src/main/java/cn/cyyaw/util/buildcode/controller/CodeController.java
@@ -34,34 +34,37 @@ public class CodeController {
         if (null != tableList) {
             for (int i = 0; i < tableList.size(); i++) {
                 JavaData javaData = tableList.get(i);
-                List<JavaColumn> javaColumns = javaData.getJavaColumns();
-                Map<String, Object> map = new HashMap();
-                //===========================================
-                map.put("basePackage", "cn.cyyaw.weixin.admin");          //基础包
-                map.put("basePathVue", "");                   //基础路径
-                //===========================================
-                map.put("javaData", javaData);                        //数据表信息
-                map.put("javaColumns", javaColumns);    //数据字段列表
-                map.put("vueJsons", TypeTools.javaColumnList2VueJsonList(javaColumns));
-                //===========================================
-                map.put("operationTools", operationTools);//工具类
-                map.put("interfaceToos", interfaceToos);//工具类
-                map.put("__Table__", operationTools.indexToUpperCase(javaData.getTable()));// 表名首字母大写
-                map.put("__table__", operationTools.indexToLowerCase(javaData.getTable()));// 表名首字母小写
+                String st = javaData.getTable().toLowerCase();
+                if( !"hibernate_sequence".equals(st)){
+                    List<JavaColumn> javaColumns = javaData.getJavaColumns();
+                    Map<String, Object> map = new HashMap();
+                    //===========================================
+                    map.put("basePackage", "cn.cyyaw.weixin.admin");          //基础包
+                    map.put("basePathVue", "");                   //基础路径
+                    //===========================================
+                    map.put("javaData", javaData);                        //数据表信息
+                    map.put("javaColumns", javaColumns);    //数据字段列表
+                    map.put("vueJsons", TypeTools.javaColumnList2VueJsonList(javaColumns));
+                    //===========================================
+                    map.put("operationTools", operationTools);//工具类
+                    map.put("interfaceToos", interfaceToos);//工具类
+                    map.put("__Table__", operationTools.indexToUpperCase(javaData.getTable()));// 表名首字母大写
+                    map.put("__table__", operationTools.indexToLowerCase(javaData.getTable()));// 表名首字母小写
 
-                map.put("__Pk__", operationTools.indexToUpperCase(operationTools.getPK(javaColumns).getColumnName()));// 主键首字母大写
-                map.put("__pk__", operationTools.indexToLowerCase(operationTools.getPK(javaColumns).getColumnName()));// 主键首字母小写
-                map.put("__pk_all__", operationTools.allToLowerCase(operationTools.getPK(javaColumns).getColumnName()));// 主键all母小写
+                    map.put("__Pk__", operationTools.indexToUpperCase(operationTools.getPK(javaColumns).getColumnName()));// 主键首字母大写
+                    map.put("__pk__", operationTools.indexToLowerCase(operationTools.getPK(javaColumns).getColumnName()));// 主键首字母小写
+                    map.put("__pk_all__", operationTools.allToLowerCase(operationTools.getPK(javaColumns).getColumnName()));// 主键all母小写
 
-                map.put("__pkJava__", operationTools.getPK(javaColumns).getJavaType());// 主键java首字母大写
+                    map.put("__pkJava__", operationTools.getPK(javaColumns).getJavaType());// 主键java首字母大写
 
-                //===========================================  常用变量
-                createCode.setDataMap(map);
-                System.out.println("正在生成文件。。。:" + tableList.get(i).getTableNote());
-                if (createCode.out()) {
-                    System.out.println("生成文件成功");
-                } else {
-                    System.out.println("生成文件失败");
+                    //===========================================  常用变量
+                    createCode.setDataMap(map);
+                    System.out.println("正在生成文件。。。:" + tableList.get(i).getTableNote());
+                    if (createCode.out()) {
+                        System.out.println("生成文件成功");
+                    } else {
+                        System.out.println("生成文件失败");
+                    }
                 }
             }
         }
